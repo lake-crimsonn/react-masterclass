02b520cb94ef15e196534a227f2f56f82a9e711a
lake-crimsonn <lakecrimsonn@gmail.com>
2023-07-02 15:08:01 +0900

    basic usage

--- README.md
+++ README.md
@@ -4,4 +4,27 @@
 
 - npm i styled-components
 
-##
+## 플러그인
+
+- vscode-styled-components
+
+## 1-styled-com 브랜치
+
+- styled.div``;
+  - 기본 사용법
+- \${(prop)=>props.bgColor}
+  - 부모의 프로퍼티를 가져올 수 있다
+- styled(Box)``;
+  - 같은 스타일 컴포넌트를 상속 받아서 코드를 재사용한다
+- <Btn as="a" href="https://~"> as
+  - 굳이 다른 컴포넌트를 만들지 않아도 엘레멘트에서 태그 변경 가능
+- stypled.input.attrs({required:true}) attrs
+  - 공통 attrs 갖는 태그 생성
+- animations
+  - keyframes
+- pseudo selector
+  - state selector
+- theme
+  - ThemeProvider 안에 있는 태그는 theme을 통해서 같은 컬러를 공유할 수 있게 된다
+
+---
--- src/App.js
+++ src/App.js
@@ -1,16 +1,109 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
+
+const Mother = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
+// 0 Box를 상속받아서 사용
 const Circle = styled(Box)`
   border-radius: 50px;
+`;
+
+// 1 버튼을 a태그로 변경해서 사용
+const Btn = styled.button`
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+// 2 input의 attrs를 포함하는 태그 생성할 수 있다
+const Attr = styled.input.attrs({
+  required: true,
+})`
+  background-color: wheat;
+`;
+
+// 3 keyframes를 이용한 애니메이션
+const rotateAnimation = keyframes`
+  /* from {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  to {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  } */
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px; 
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+// 5 pseudo selector
+const Emoji = styled.span`
+  font-size: 40px;
+`;
+
+// 3 animation 변수를 가져와서 박스에 적용
+const AniBox = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotateAnimation} 3s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  // 4 AniBox 안에 있는 span을 추적할 수 있다
+  span {
+    font-size: 40px;
+    // span:hover
+    &:hover {
+      font-size: 80px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+
+  // 5 Emoji pseudo selector
+  ${Emoji} {
+    &:hover {
+      font-size: 80px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
+// 6 index.js에 있는 ThemeProvider.theme를 통해서 부모의 색상을 가져올 수 있다
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+// 6 index.js에 있는 ThemeProvider.theme를 통해서 부모의 색상을 가져올 수 있다
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
@@ -19,8 +112,31 @@ function App() {
       <Father>
         <Box bgColor="tomato"></Box>
         <Box bgColor="teal"></Box>
+        {/* 0 */}
         <Circle bgColor="wheat"></Circle>
       </Father>
+
+      {/* 1 */}
+      <Btn as="a" href="/">
+        click
+      </Btn>
+      {/* 2 */}
+      <Attr></Attr>
+
+      <Mother>
+        {/* 3 */}
+        <AniBox>
+          {/* 4 */}
+          <span>🙂</span>
+          {/* 5 */}
+          <Emoji>🙂</Emoji>
+        </AniBox>
+      </Mother>
+
+      {/* 6 */}
+      <Wrapper>
+        <Title>Hello</Title>
+      </Wrapper>
     </div>
   );
 }
