88e4c5d5c10241e92235729ed1a614830b9c1c58
lake-crimsonn <lakecrimsonn@gmail.com>
2023-07-02 11:46:32 +0900

    2.2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# 노마드코더 리액트 마스터 클래스
+# styled components
 
-- styled component
+## 설치
+
+- npm i styled-components
+
+##
--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-// npm i styled-component
-
-import styled from "styled-component";
+import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled`
-  background-color: ${(props) => props.bgColor}
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <Father>
         <Box bgColor="tomato"></Box>
         <Box bgColor="teal"></Box>
+        <Circle bgColor="wheat"></Circle>
       </Father>
     </div>
   );
