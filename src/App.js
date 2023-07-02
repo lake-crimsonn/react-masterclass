4ef94384247788881ba3c172329df575d15a7fa7
lake-crimsonn <lakecrimsonn@gmail.com>
2023-07-02 11:17:44 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// npm i styled-component
+
+import styled from "styled-component";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled`
+  background-color: ${(props) => props.bgColor}
+  width: 100px;
+  height: 100px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Father>
+        <Box bgColor="tomato"></Box>
+        <Box bgColor="teal"></Box>
+      </Father>
     </div>
   );
 }
